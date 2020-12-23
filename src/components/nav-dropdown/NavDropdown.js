25f820bef8c1631798e12bfc3e44101d82156c56
Teebreezy101 <tammy.batubo@gmail.com>
2020-12-23 14:43:13 +0100

    add social media links and add resume

--- src/components/nav-dropdown/NavDropdown.js
+++ src/components/nav-dropdown/NavDropdown.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './nav-dropdown.scss';
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as Wave } from "../../assets/wave.svg";
+import { ReactComponent as Wave } from '../../assets/wave.svg';
+import Resume from "../../assets/TammyBatuboResume.pdf";
 
 const NavDropdown = () => {
   return (
@@ -9,30 +10,54 @@ const NavDropdown = () => {
       <div className="nav-dropdown__content">
         <div className="nav-links">
           <div className="nav-item-container">
-            <NavLink activeClassName="active" className="nav-item" to="/">
-              HOME
+            <NavLink activeClassName="active" className="nav-item" exact to="/">
+              Home
             </NavLink>
           </div>
           <div className="nav-item-container">
-            <NavLink className="nav-item" to="/projects" exact>
-              PROJECTS
+            <NavLink className="nav-item" to="/projects">
+              Projects
             </NavLink>
           </div>
           <div className="nav-item-container">
             <NavLink className="nav-item" to="/contact">
-              CONTACT
+              Contact
             </NavLink>
           </div>
           <div className="nav-item-container">
-            <a className="nav-item resume" download>
-              RESUME
+            <a href={Resume} className="nav-item" download="TammyBatuboResume">
+            <i class="fas fa-file-download"></i>
+              Resume
             </a>
           </div>
         </div>
-        <div className="nav-socials"></div>
+        <div className="nav-socials">
+          <div className="nav-icons-container">
+            <a
+              href="https://www.linkedin.com/in/tammy-batubo-4023b6119/"
+              className="nav-icons"
+              target="_blank"
+            >
+              <i class="fab fa-linkedin"></i>
+              <span className="nav-socials__text">Tammy-Batubo</span>
+            </a>
+            <a
+              href="https://twitter.com/dev_TamB"
+              className="nav-icons"
+              target="_blank"
+            >
+              <i class="fab fa-twitter-square"></i>
+              <span className="nav-socials__text">@dev_TamB</span>
+            </a>
+            <a href="mailto:tammy.batubo@gmail.com" className="nav-icons">
+              <i class="fab fa-google-plus-square"></i>
+              <span className="nav-socials__text">Tammy.batubo@gmail.com</span>
+            </a>
+          </div>
+        </div>
       </div>
       <div className="nav-dropdown__waves">
-          <Wave />
+        <Wave />
       </div>
     </div>
   );
