99771fc056275fcde573cf12bf0f20cfaf226135
Teebreezy101 <tammy.batubo@gmail.com>
2020-12-26 07:43:26 +0100

    add waves and remove blob on project page

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './row.scss';
 
-const Row = ({image, desc}) => {
-    return (
-        <div className="row">
-            <h1>{desc}</h1>
-            <img src={image} alt={desc} className="row__mockup"/>
+const Row = ({ image, desc }) => {
+  return (
+    <div className="row">
+      <div className="row__image">
+        <img src={image} alt={desc} className="row__mockup" />
+      </div>
+      <div className="row__content">
+        <h2 className="row__title">{desc}</h2>
+        <p className="row__desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In eum fugit
+          corporis iusto earum quae similique voluptate sit illo tempore.
+        </p>
+        <div className="row__buttons">
+          <a href="" className="row__btn">
+            Demo site
+          </a>
+          <a href="" className="row__btn">
+            View on Github
+          </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Row
+export default Row;
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react';
 import { undoCanvas } from '../../utils/undoCanvas';
 import './projects.scss';
-import projectDetails from "../../utils/projectDetails";
+import projectDetails from '../../utils/projectDetails';
 import Row from '../../components/row/Row';
 
 const Projects = () => {
   useEffect(() => {
     undoCanvas();
   }, []);
-
 
   return (
     <div className="projects">
@@ -18,15 +17,23 @@ const Projects = () => {
         </h1>
 
         <div className="projects__display">
-            {
-                projectDetails.map((project, idx) => {
-                    return (
-                        <Row image={project.image} desc={project.desc} key={idx} />
-                    )
-                })
-            }
+          {projectDetails.map((project, idx) => {
+            return <Row image={project.image} desc={project.desc} key={idx} />;
+          })}
         </div>
       </div>
+      {/* <div className="blob1"></div> */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="projects__waves"
+      >
+        <path
+          fill="#181102"
+          fill-opacity=".7"
+          d="M0,160L60,138.7C120,117,240,75,360,64C480,53,600,75,720,122.7C840,171,960,245,1080,261.3C1200,277,1320,235,1380,213.3L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
     </div>
   );
 };
